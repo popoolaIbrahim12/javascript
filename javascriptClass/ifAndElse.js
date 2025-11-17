80f68179b4833274507cea0ad22aead27eb87ecf
popoolaIbrahim12 <ibrahimpopoola926@gmail.com>
2025-11-17 11:11:11 +0100

    if and else

---
+++ javascriptClass/ifAndElse.js
@@ -0,0 +1,39 @@
+//
+let age = 15
+if (age >= 18) {
+    console.log("you can vote");
+    
+}else{
+ console.log("you are not up to 18");
+ 
+}
+
+let score = 59
+
+if( score >= 75){
+console.log("you grade is A");
+
+} else if(score >= 60){
+    console.log("your grade is B");
+    
+} else if (score >= 50 ){
+    console.log("your grade is C");
+    
+}else {
+    console.log("your grade is F");
+    
+}
+
+//
+
+let weather = 10
+if (weather >= 35) {
+    console.log("the weather is too hot");
+    
+} else if (weather >= 20 ) {
+    console.log("the weather is too cold");
+    
+}else {
+    console.log("the weather is okay");
+    
+}
